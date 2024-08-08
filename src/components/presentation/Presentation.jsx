9f285f719a8fd4bf503e3c9b17b28c6c0ccada65
josuesss72 <jjdanielsarmiento@gmail.com>
2024-08-08 12:39:12 -0500

    josue sarmiento vides => add animations with framer motion in the component presentations

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -1,16 +1,49 @@
 import React from 'react'; 
 import styles from './Presentation.module.css'
+import { motion } from 'framer-motion';
+import {useAnimations} from '../../hooks/useAnimations';
 
 const Presentation = () => {
+  const { transitionRight, fade, transitionLeft, transitionBottom} = useAnimations() 
+
   return (
     <section className={styles.container}>
-      <article className={styles.containerImg}>
+      <motion.div 
+        className={styles.containerImg}
+        initial={fade.initial}
+        animate={fade.animate}
+        transition={{delay: 0.5, duration: 2 }} 
+      >
         <img className={styles.image} src="/Avatars/waving doorway.png" alt="" />
-      </article>
+        <div className={styles.mask}></div>
+        <div className={styles.mask}></div>
+      </motion.div>
       <article className={styles.containerText}>
-        <h2 className={styles.subtitle}>Hi, my name is</h2>
-        <h1 className={styles.title}>Josue Sarmiento</h1>
-        <h3 className={styles.text}>Developer <span className={styles.highlighted}>Web full Stack</span></h3>
+        <motion.h2 
+          className={styles.subtitle}
+          initial={transitionRight.initial}
+          animate={transitionRight.animate}
+          transition={{ delay: 1, duration: 1 }}
+        >
+          Hi, my name is
+        </motion.h2>
+        <motion.h1 
+          className={styles.title}
+          initial={transitionLeft.initial}
+          animate={ transitionLeft.animate }
+          transition={{ delay: 1.5, duration: 1 }}
+        >
+          Josue Sarmiento
+        </motion.h1>
+        <motion.h3 
+          className={styles.text}
+          initial={transitionBottom.initial}
+          animate={transitionBottom.animate}
+          transition={{ delay: 2, duration: 1 }}
+        >
+          Developer 
+          <span className={styles.highlighted}>Web full Stack</span>
+        </motion.h3>
       </article> 
     </section>	
   )
