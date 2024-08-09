ba6835902033e505a75429e6816b493c61e79657
josuesss72 <jjdanielsarmiento@gmail.com>
2024-08-09 12:05:20 -0500

    josue sarmiento => i modified the configuration of route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import styles from './App.module.css'
-import Layout from './components/layout/Layout'
 import Presentation from './components/presentation/Presentation'
-import Home from './pages/Home/Home'
+import { Routes, Route } from 'react-router'
+import ProyectsPage from './pages/ProyectsPage/ProyectsPage'
 
 // App component that encompasses layout and routing 
 function App() {
   return (
     <div className={styles.container}>
-      <Presentation/>
-      <Layout>
-        <Home/>
-      </Layout>
+      <Routes>
+        <Route path='/' element={<Presentation/>}/>
+        <Route path='/proyects' element={<ProyectsPage/>}/> 
+      </Routes>
     </div> 
   )
 }
---
+++ src/pages/ProyectsPage/ProyectsPage.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./ProyectsPage.module.css"
+import Layout from "../../components/layout/Layout"
+
+const ProyectsPage = () => {
+	return (
+		<Layout>
+			<main>
+				<h1>ProyectsPage</h1>
+			</main>
+		</Layout>	
+	)
+}
+
+export default ProyectsPage 
